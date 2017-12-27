fb2921217afda86adde31d61f992c605556419f1
Dylan Tran <tiendatdeveloper1997@gmail.com>
2017-12-27 14:03:47 +0700

    Tien Dat Yeu Vau
    Edit Page : Update Event

--- src/main/resources/static/js/adminEventEdit.js
+++ src/main/resources/static/js/adminEventEdit.js
@@ -1,5 +1,8 @@
 $('#daterange').daterangepicker({
-	        autoApply:true
+	autoApply:true,
+	locale: {
+		format: 'MM/DD/YYYY'
+	}
 });
 
 
@@ -17,3 +20,57 @@ $('#datetime').daterangepicker({
     }  
 });
 
+
+
+
+$('#btn-update-event').on('click', function(){
+	
+	var eventId = $('#eventId').val();
+	console.log(eventId);
+	var name = $("#eventName").val();
+	var date = $("#daterange").val();
+	var description = $("#eventDescription").val();
+	var isPublic = $('#cb-public').prop('checked');
+	
+	var notPublic = !isPublic;
+
+	var tokenDate = date.split(' - '); 
+	var dateStart = tokenDate[0]; 
+	var dateEnd = tokenDate[1]; 
+
+	console.log(name);
+	console.log(dateStart);
+	console.log(dateEnd);
+	console.log(description);
+	console.log(notPublic);
+
+	
+
+	var request = $.ajax({
+		url: '/admin/api/event/update',
+		data: {
+			eventId: eventId,
+			name: name,
+			dateStart: dateStart,
+			dateEnd: dateEnd,
+			description: description,
+			notPublic: notPublic
+		},
+		cached: false
+	});
+
+	request.done(function(msg){
+		console.log(msg);		
+	});
+
+	request.fail(function(msg){
+		console.log(msg);
+	});
+
+
+
+
+
+	
+
+});
